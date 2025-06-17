385fd4d99c48402f874a93d101f91a5bc8c535de
bcollignon <bertrand.collignon@ac-bordeaux.fr>
2025-06-17 12:47:05 +0200

    fix mars

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('This is version 3 of the app.' + '\n');
 });
 
-app.get('/', function (req, res) {
+app.get('/mars', function (req, res) {
   res.send('Hello Mars\n');
 });
 
